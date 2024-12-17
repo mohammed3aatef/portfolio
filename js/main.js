6ba71f3d2a8f8791cbe5308bf7271c9021307b9d
Mohamed Atef <160427157+mohammed3aatef@users.noreply.github.com>
2024-12-17 23:04:18 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.nav-link');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.nav-link[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
